2aef3197a5f3151e54810b6c2464107fa60de91c
CuteTN <quantiennghia001@gmail.com>
2021-02-07 02:46:51 +0700

    Build frame for colors and fonts palletes

---
+++ cutetn-page/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import { palletes } from './styles/pallete/allPalletes';
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+      </header>
+    </div> 
+  );
+}
+
+export default App;
---
+++ cutetn-page/src/styles/pallete/colors.js
@@ -0,0 +1,9 @@
+export const colors = {
+    white: '#ffffff',
+    black: '#000000',
+
+    pink0: '#ffe5eb',
+    pink1: '#ffccd7',
+    pink2: '#ff82c0',
+    pink3: '#ff2a94',
+ }
